c20a0869e1cc94e27dc3d04e592b6a5dbe5f2104
James Bogosian <bogosj@gmail.com>
2020-09-21 10:51:06 -0400

    Module to read a config file.

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// Config is the main binary configuration.
+type Config struct {
+	ClientID     string
+	ClientSecret string
+	Email        string
+	Password     string
+}
+
+// New returns a Config from a json file at the provided path.
+func New(path string) Config {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var cfg Config
+	err = json.Unmarshal(bytes, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cfg
+}
